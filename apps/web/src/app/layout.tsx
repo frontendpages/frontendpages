0c051d64650afb1ee82c9b5bc845de6fd51081f4
Hashversion <110537156+Hashversion@users.noreply.github.com>
2026-02-04 19:21:10 +0530

    feat: add a banner component to the layout for frontend page access notification

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -46,9 +46,25 @@ export default function RootLayout({
           transition={{ duration: 1.23 }}
           className="font-geist antialiased"
         >
-          <Providers>{children}</Providers>
+          <Providers>
+            <Banner />
+            {children}
+          </Providers>
         </m.body>
       </LazyMotion>
     </html>
+  );
+}
+
+function Banner() {
+  return (
+    <div className="sticky top-0 hidden flex-row items-center justify-center bg-black px-8 py-2 text-sm text-white md:flex dark:bg-white dark:text-black">
+      <p>
+        You can also access frontendpages.com via {""}
+        <a href="https://frontend.page" className="font-bold hover:opacity-80">
+          frontend.page
+        </a>
+      </p>
+    </div>
   );
 }
