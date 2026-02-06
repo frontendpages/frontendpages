3faf36b26206a31937aaab8c59d77221fee73328
Hashversion <110537156+Hashversion@users.noreply.github.com>
2026-02-06 19:51:03 +0530

    feat: add social media icons to the frontend page and adjust header title size

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -8,11 +8,29 @@ export default function Page() {
           <div className="space-y-3 text-center">
             <div className="flex flex-col items-center justify-center gap-8">
               <Icons.Logo width={100} height={100} />
-              <h1 className="font-krylon text-8xl">Frontendpages</h1>
+              <h1 className="font-krylon text-6xl">Frontendpages</h1>
             </div>
           </div>
         </div>
       </header>
+      <div>
+        <SocialIcons />
+      </div>
+    </>
+  );
+}
+
+function SocialIcons() {
+  return (
+    <>
+      <div className="flex justify-center gap-3">
+        <a href={"https://x.com/intent/follow?screen_name=frontendpages"} target="_blank" rel="noreferrer">
+          <Icons.X className="hover:opacity-80" />
+        </a>
+        <a href={"https://www.instagram.com/frontendpages/"} target="_blank" rel="noreferrer">
+          <Icons.Instagram className="hover:opacity-80" />
+        </a>
+      </div>
     </>
   );
 }
