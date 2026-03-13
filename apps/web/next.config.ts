c3d5e820ac0995b7d6db3f3e66de8b752f3ecbfe
Hashversion <110537156+Hashversion@users.noreply.github.com>
2026-03-13 11:34:07 +0530

    feat: move posthog next.js proxy rewrites to posthog managed proxy

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -10,22 +10,6 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
   cacheComponents: false,
   typedRoutes: true,
-
-  async rewrites() {
-    return [
-      // posthog proxy
-      {
-        source: "/_proxy/posthog/:path*",
-        destination: "https://us.i.posthog.com/:path*",
-      },
-      {
-        source: "/_proxy/posthog/static/:path*",
-        destination: "https://us-assets.i.posthog.com/static/:path*",
-      },
-    ];
-  },
-  // This is required to support PostHog trailing slash API requests
-  skipTrailingSlashRedirect: true,
 };
 
 export default bundleAnalyzer(nextConfig);
