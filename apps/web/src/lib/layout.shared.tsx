1647cba956846ea28cf678de0ba94475967f7575
Hashversion <110537156+Hashversion@users.noreply.github.com>
2026-03-19 12:47:52 +0530

    style: tweak logo spacing and app title font

--- apps/web/src/lib/layout.shared.tsx
+++ apps/web/src/lib/layout.shared.tsx
@@ -6,11 +6,11 @@ export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
       title: (
-        <div className="flex items-center justify-center gap-2">
-          <div className="corner-squircle rounded-[50] bg-black p-1 text-white dark:bg-white dark:text-black">
+        <div className="flex items-center justify-center gap-3">
+          <div className="bg-black p-1.5 text-white dark:bg-white dark:text-black">
             <Icons.Logo className="size-3" />
           </div>
-          <p>{config.app}</p>
+          <p className="font-neutral-sans">{config.app}</p>
         </div>
       ),
     },
