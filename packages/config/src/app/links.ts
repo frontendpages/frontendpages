177f876d5c7b0d124b1a65bc341da671fb6b0b12
Hashversion <110537156+Hashversion@users.noreply.github.com>
2026-05-23 15:42:46 +0530

    feat: introduce new config package with initial setup and SEO components

---
+++ packages/config/src/app/links.ts
@@ -0,0 +1,12 @@
+export const links = {
+  base: "https://frontendpages.com",
+  cdn: "https://cdn.frontendpages.com",
+  og: `https://cdn.frontendpages.com/brand/opengraph-image.png`,
+  logo: "https://cdn.frontendpages.com/brand/logo.png",
+  github: "https://github.com/frontendpages/frontendpages",
+  facebook: "https://facebook.com/frontendpages",
+  x: "https://x.com/frontendpages",
+  instagram: "https://instagram.com/frontendpages",
+  linkedin: "https://linkeidn.com/company/frontendpages",
+  bluesky: "https://bsky.app/profile/frontendpages.com",
+};
---
+++ packages/config/src/app/site.ts
@@ -0,0 +1,11 @@
+export const site = {
+  name: "Frontend Pages",
+  slug: "frontendpages",
+  domain: {
+    main: "frontendpages.com",
+    redirect: "frontend.page",
+  },
+  tagline: "",
+  handle: "@frontendpages",
+  description: "",
+};
